38c22024f23b893459f9bf4d84133f42db7c7bb6
Calvin James T. Maximo <calvin.james.12391@gmail.com>
2025-03-19 15:29:31 +0800

    feat: placeholder text in landing page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,31 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { useEffect } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
 import { Stack } from "expo-router";
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { StatusBar } from 'expo-status-bar';
 import "@/global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+      SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    });
+  
+    useEffect(() => {
+      if (loaded) {
+        SplashScreen.hideAsync();
+      }
+    }, [loaded]);
+  
+    if (!loaded) {
+      return null;
+    }
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack />
+      <StatusBar style="auto"/>
+    </ThemeProvider>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -3,13 +3,9 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      className="flex-1 justify-center items-center"
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-pink-500 text-2xl">Centennial Residence Hall: Better QR Scanner</Text>
     </View>
   );
 }
