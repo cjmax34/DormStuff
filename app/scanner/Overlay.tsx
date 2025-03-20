0f156a860aaae3be5daa6034c009185c4d0ba2a8
Calvin James T. Maximo <calvin.james.12391@gmail.com>
2025-03-20 18:40:05 +0800

    feat: add Overlay component

--- app/scanner/Overlay.tsx
+++ app/scanner/Overlay.tsx
@@ -1,9 +1,30 @@
-import { View, Text } from "react-native";
+import { Canvas, DiffRect, rect, rrect } from "@shopify/react-native-skia";
+import { Dimensions, Platform, StyleSheet } from "react-native";
 
-export default function Overlay() {
+const { width, height } = Dimensions.get("window");
+
+const innerDimension = 300;
+
+const outer = rrect(rect(0, 0, width, height), 0, 0);
+const inner = rrect(
+  rect(
+    width / 2 - innerDimension / 2,
+    height / 2 - innerDimension / 2,
+    innerDimension,
+    innerDimension
+  ),
+  50,
+  50
+);
+
+export const Overlay = () => {
   return (
-    <View>
-      <Text>Overlay</Text>
-    </View>
+    <Canvas
+      style={
+        Platform.OS === "android" ? { flex: 1 } : StyleSheet.absoluteFillObject
+      }
+    >
+      <DiffRect inner={inner} outer={outer} color="black" opacity={0.5} />
+    </Canvas>
   );
-}
+};
