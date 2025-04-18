71796b719f0cb5bbf2055017100743d8d8a58fb6
Calvin James T. Maximo <calvin.james.12391@gmail.com>
2025-04-18 16:42:10 +0800

    refactor: move custom interfaces to a single file

---
+++ types/index.ts
@@ -0,0 +1,22 @@
+import { Href } from "expo-router";
+
+export interface Resident {
+  id: number;
+  name: string;
+  room: string;
+  isIn: boolean;
+  time: string;
+}
+
+export interface GlobalContextType {
+  residents: Resident[];
+  setResidents: React.Dispatch<React.SetStateAction<Resident[]>>;
+}
+
+export interface Action {
+  id: number;
+  title: string;
+  description: string;
+  icon: string;
+  path: Href;
+}
