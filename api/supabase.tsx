b146254901c56103a185be0f60d2b85d1b2e1436
Calvin James T. Maximo <calvin.james.12391@gmail.com>
2025-04-26 21:00:59 +0800

    feat: include filter in realtime table subscription

--- api/supabase.tsx
+++ api/supabase.tsx
@@ -1,18 +1,22 @@
 import { supabase } from "@/lib/supabase";
 
+interface SubscriptionProps {
+  table: string;
+  event?: any;
+  schema?: string;
+  filter?: string;
+  callback: (payload: any) => void;
+}
+
 export function subscribeToTableChanges({
   table,
   event = "*",
   schema = "public",
+  filter,
   callback,
-}: {
-  table: string;
-  event?: any;
-  schema?: string;
-  callback: (payload: any) => void;
-}) {
+}: SubscriptionProps) {
   return supabase
     .channel(`${table}-${event}`)
-    .on("postgres_changes", { event, schema, table }, callback)
+    .on("postgres_changes", { event, schema, table, filter }, callback)
     .subscribe();
 }
