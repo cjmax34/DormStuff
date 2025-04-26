b946b11bc2fde66f36ebaa737b57bc7211beef0c
Calvin James T. Maximo <calvin.james.12391@gmail.com>
2025-04-26 22:16:59 +0800

    fix!: hehe

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,9 +1,9 @@
-import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
+import 'react-native-url-polyfill/auto';
 
-const supabaseUrl = "https://lxyiaubycqxuhwrbxdtt.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4eWlhdWJ5Y3F4dWh3cmJ4ZHR0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5OTE5NTMsImV4cCI6MjA2MDU2Nzk1M30.XznjlBxCnbbd3Y5CXSf_BldkRtBsYTmUBRk2qPravyw";
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || '';
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON || '';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
